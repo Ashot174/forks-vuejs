5fe886eb1c7cc065faeeb5553af3835beaa96b31
mkrtumabrahamyan <mkrtumabrahamyan12@gmail.com>
2021-06-08 19:46:39 +0400

    first commit

---
+++ dist/precache-manifest.7559c80abf64f978b511b143816c792c.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c35307225688af3a7ae7",
+    "url": "/css/app.16dabf3b.css"
+  },
+  {
+    "revision": "a443a07369bf5100000c",
+    "url": "/css/chunk-vendors.52eaf30c.css"
+  },
+  {
+    "revision": "8a16e86b938370dbec95689720fb060e",
+    "url": "/index.html"
+  },
+  {
+    "revision": "c35307225688af3a7ae7",
+    "url": "/js/app.094cd708.js"
+  },
+  {
+    "revision": "a443a07369bf5100000c",
+    "url": "/js/chunk-vendors.910afff7.js"
+  },
+  {
+    "revision": "3c1397dbf5c9c87dae884ce098533cdf",
+    "url": "/manifest.json"
+  },
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "/robots.txt"
+  }
+]);
